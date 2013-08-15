39d4b20b832c3f4f4026202b332ec0d6df940af8
robertdalum <robertdalum@yahoo.com>
2013-08-15 17:07:13 -0500

    Procedures

--- dalum_robert_assignment2.js
+++ dalum_robert_assignment2.js
@@ -1,1 +1,20 @@
 alert("JavaScript works!");
+
+// Robert Dalum
+// SDI0813
+//Assignment 2
+
+var anticipatedGame = ["Call of Duty: Ghosts"];
+var stores = ["Gamestop, Best Buy, Walmart"];
+var quantity = [100, 75, 200];
+var daysLeft = 2
+
+//Procedure
+var getStores = function (storesQuantity){
+	var averageQuantity = 125;
+	if (averageQuantity < storesQuantity){
+		console.log("If the prestige edition has limited quantities I have to rush to the store and pre-order!");
+	}else{
+		console.log("I have plenty of time to pre-order.");
+	}
+};
